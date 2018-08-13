3a8e3b68ebf750f2c622ffd8375a86c681bc84e1
ling <1007890788@qq.com>
2018-08-13 09:37:56 +0800

    finished lottery

--- app/js/index.js
+++ app/js/index.js
@@ -1,3 +1,6 @@
 // import './class/lesson-19';
-import lesson from './class/lesson-19';
-console.log(lesson.a);
+// import lesson from './class/lesson-19';
+// console.log(lesson.a);
+// es兼容
+import 'label-polyfill';
+import Lottery from './lottery';
---
+++ app/js/lottery.js
@@ -0,0 +1,4 @@
+import './lottery/base.js';
+import './lottery/timer.js';
+import './lottery/interface.js';
+import './lottery/calculate.js';
